038ebaa66c5487be3d09ce4066221853c49a2dcd
Trung <trung.duong@parcelperform.com>
2023-05-17 15:52:44 +0700

    chore: remove build

--- src/packages/return/build/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-var ReturnComponent = function () {
-    return React.createElement("div", null, "ReturnComponent");
-};
-
-export { ReturnComponent };
-//# sourceMappingURL=index.js.map
--- src/packages/shipment/build/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-var ShipmentComponent = function ShipmentComponent() {
-  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Shipment Component"));
-};
-
-export { ShipmentComponent };
-//# sourceMappingURL=index.js.map
