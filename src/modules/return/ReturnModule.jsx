e5304d54155a292dc6f7a486a41ec835a0cbb10f
Trung <trung.duong@parcelperform.com>
2023-05-20 15:27:17 +0700

    chore: remove eject assets

--- src/modules/return/ReturnModule.jsx
+++ src/modules/return/ReturnModule.jsx
@@ -1,4 +1,4 @@
-import { ReturnComponent } from "@foo-testing-0103/return/index";
+import { ReturnComponent } from "@foo-testing-0103/return";
 
 export const ReturnModule = () => {
   return <ReturnComponent />;
